dd9a3d28fe45f259c33e3aa8f7934651858a0724
pritam195 <chavanpritam172@gmail.com>
2025-07-03 13:05:16 +0530

    env file update

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+require("dotenv").config();
 
 const userModel = require("./models/user")
 const passwordModel = require('./models/password')
@@ -10,14 +11,23 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("✅ MongoDB connected"))
+  .catch((err) => console.error("❌ MongoDB error:", err));
+
+  
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
 app.use(cors({
     origin: "http://localhost:5173",
     credentials: true 
 }))
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser());
 
 app.get("/", (req, res) => {
     res.send("HELLO WORLD");
@@ -172,4 +182,5 @@ app.post("/contact", async (req, res) => {
 
 
 
-app.listen(3000);
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
--- backend/models/user.js
+++ backend/models/user.js
@@ -1,6 +1,5 @@
-const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://127.0.0.1:27017/passmanagar");
+const mongoose = require("mongoose");
 
 const userSchema = mongoose.Schema({
   username: String,
