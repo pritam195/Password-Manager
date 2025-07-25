16c51efa8b89aea704fb3be7f3d8b8a90dd54ab0
pritam195 <chavanpritam172@gmail.com>
2025-07-25 23:27:21 +0530

    deploying

--- backend/index.js
+++ backend/index.js
@@ -26,7 +26,7 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: "https://passwordmanager-iota.vercel.app",
+    origin: "https://password-manager-vjti.vercel.app",
     credentials: true,
   })
 );
